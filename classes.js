80e05d882cc1b27f82f0e41ce1f1e2837aee599b
Lucas Heim <lucasedfc@gmail.com>
2020-11-05 23:22:13 -0300

    Classes and Generator

---
+++ classes.js
@@ -0,0 +1,43 @@
+
+class calculator {
+    
+    
+    // Init variables
+    constructor() {
+        this.valueA = 0;
+        this.valueB = 0;
+    }
+    
+    sum(valueA, valueB) {
+        this.valueA = valueA;
+        this.valueB = valueB;
+        return this.valueA + this.valueB;
+    }
+    
+}
+
+const calc = new calculator();
+console.log(calc.sum(2,3));
+
+const hello = require("./module");
+//es6
+//import { hello } from './module'
+
+console.log(hello());
+
+// Generator
+
+function* helloWorld () {
+    if(true) {
+        yield 'Hello, ';
+    }
+    if(true) {
+        yield 'World';
+    }
+};
+
+const generatorHello = helloWorld();
+
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
